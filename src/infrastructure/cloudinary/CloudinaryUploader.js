1f54e546bb86d4d2d60c9cf38d24cec1834e8fed
Angel Sanabria <as1945228@gmail.com>
2026-05-03 14:55:57 -0600

    feat: update CloudinaryUploader configuration to use upload preset

--- src/infrastructure/cloudinary/CloudinaryUploader.js
+++ src/infrastructure/cloudinary/CloudinaryUploader.js
@@ -3,16 +3,17 @@ const ImageUploader = require('../../domain/player/ImageUploader');
 require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME
 });
 
 class CloudinaryUploader extends ImageUploader {
   async upload(file) {
     return new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
-        { folder: 'balon-de-oro' },
+        {
+          upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+          folder: 'balon-de-oro'
+        },
         (error, result) => {
           if (error) return reject(error);
           resolve(result.secure_url);
