baa9739a9ce6c1c0c475d5d64a36cc6ed0df38ff
Angel Sanabria <as1945228@gmail.com>
2026-05-03 23:17:40 -0600

    fix: update path to openapi.yaml in swagger setup

--- src/infrastructure/http/swagger/swaggerSetup.js
+++ src/infrastructure/http/swagger/swaggerSetup.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-const openapiPath = path.join(__dirname, '../swagger/openapi.yaml');
-const openapiContent = fs.readFileSync(openapiPath, 'utf8');
-const swaggerDocument = yaml.load(openapiContent);
-
 const setupSwagger = (app) => {
+  const openapiPath = path.join(__dirname, 'openapi.yaml');
+  const openapiContent = fs.readFileSync(openapiPath, 'utf8');
+  const swaggerDocument = yaml.load(openapiContent);
+
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
 
