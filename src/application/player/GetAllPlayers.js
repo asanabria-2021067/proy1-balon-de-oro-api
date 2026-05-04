c49c9b7e1e4b13c2b5bf72c1f317196a5adbcb55
Angel Sanabria <as1945228@gmail.com>
2026-05-04 10:43:11 -0600

    fix: increase default limit for player retrieval to 1000

--- src/application/player/GetAllPlayers.js
+++ src/application/player/GetAllPlayers.js
@@ -4,7 +4,7 @@ class GetAllPlayers {
   }
 
   async execute(filters) {
-    const { page = 1, limit = 10, q, nationality, sort = 'name', order = 'asc' } = filters;
+    const { page = 1, limit = 1000, q, nationality, sort = 'name', order = 'asc' } = filters;
     return await this.playerRepository.findAll({ page, limit, q, nationality, sort, order });
   }
 }
